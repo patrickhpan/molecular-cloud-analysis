29acf9215971f6bc9b5437b618693a9ba761119b
Patrick Pan <patrick.pan@patrickpan.com>
2016-12-07 10:51:21 -0500

    Added ability to filter data using fitted quadratic.

--- js/analyze.js
+++ js/analyze.js
@@ -1,9 +1,40 @@
-const Smooth = require('./libsmooth');
+const regression = require('./lib/regression');
+const poly_order = 2;
 
-function smoothPoints(data) {
-    let smoothed = Smooth(data);
+function fitted(data) {
+    let coefficients = regression('polynomial', data, poly_order).equation;
+    return ( x => coefficients.map((c, pow) => c * Math.pow(x, pow)).reduce((sum, next) => sum + next, 0)
+    ) ;
+}
+
+function fittedRMSE(data) {
+    let fit = fitted(data);
+    let errors = data.map(point => Math.abs(point[1] - fit(point[0])));
+    let sumSquaredErrors = errors
+        .map(x => Math.pow(x, 2))
+        .reduce((sum, next) => sum + next, 0);
+    let rmse = Math.sqrt(sumSquaredErrors / data.length)
+    return rmse;
+}
+
+function filter(data, minSigma = 1) {
+    let fit = fitted(data);
+    let rmse = fittedRMSE(data);
+    console.log(`rmse ${rmse}`);
+    return data.map(point => {
+        let predicted = fit(point[0]);
+        let error = Math.abs(point[1] - predicted);
+        if(error > minSigma * rmse) {
+            return point
+        } else {
+            return [point[0], predicted]
+            // return null
+        }
+
+        // return point[1] > rms * minSigma ? point : null
+    }).filter(x => x !== null)
 }
 
 module.exports = {
-    smoothPoints
+    fittedRMSE, filter
 }
--- test.js
+++ test.js
@@ -8,5 +8,19 @@ const fs = require('fs');
 // fs.writeFileSync('log', JSON.stringify(getData(readLines('data/Friday/galaxy_1.txt')), null, 2))
 
 const readData = require('./js/readData');
+const analyze = require('./js/analyze');
 
-fs.writeFileSync('log', JSON.stringify(readData("data/"), null, 2))
+// fs.writeFileSync('log', JSON.stringify(readData("data/").map(entry => {
+//     entry.data = analyze.filter(entry.data);
+//     return entry;
+// }), null, 2))
+
+fs.writeFileSync('log', 
+    readData('data/').map(entry => 
+        `Longitude: ${entry.longitude}\n` + 
+        analyze.filter(entry.data)
+            .map(point => 
+                `${point[0].toPrecision(4)}\t${point[1].toPrecision(4)}`
+            ).join('\n')
+    ).join('\n\n')
+);
